011e662ef2b26bb2780099c127830683eeda8914
Defhef <105176513+Defhef@users.noreply.github.com>
2023-06-27 17:37:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+    console.log('Ready')
+
+    //  Busque a data atual e atualize-a no DOM
+    $("#display_date").html(display_date)
+    //disbilitado enquanto o texto nao for digitado pelo usuario
+    $('#save_button').prop('disabled', true);
+  
+
+
+    // Escreva um evento, quando o botão Enviar for clicado
+    $('').click(function(){
+
+        //  Obtenha o valor do texto da área de texto usando o método 'val()'
+        let text_value = $('').val()
+
+        //  Converta-o em um objeto JS.
+        //  Forneça uma "chave" aqui e escreva o mesmo no arquivo app.py também para extrair dados
+        let input_text = {'' : text_value}
+        console.log(input_text)
+
+        //  requisição ajax
+        $.ajax({
+
+            //  tipo da requisição web
+            type : 'POST',
+
+            //  dados a serem enviados no formato JSON
+            data : JSON.stringify(input_data),
+
+            //  o tipo de resposta esperado é json
+            dataType : 'json',
+
+            //  contentType
+            contentType : 'application/json',
+
+            //  se tudo funcionar, execute esta função
+            success : function(result){
+
+                // extraia previsão e a URL do emoticon do resultado
+                $("#prediction").html(result.data.predicted_emotion)                
+                //  atualize os elementos DOM
+                $("#emo_img_url").attr('src', result.data.predicted_emotion_img_url);
+              
+                $('#prediction').css("display", "");
+                $('#emo_img_url').css("display", "");
+                predicted_emotion = result.data.predicted_emotion
+                //  exiba-os
+                $('#save_button').prop('disabled', false);
+            },
+
+            //  se houver algum erro, execute esta função
+            error : function(result){
+
+                console.log(result)
+            }
+        })
+
+
+        //  limpando a caixa de texto após cada pressionamento de botão
+        $('#text').val("")
+    })
+        
+})
